43f094cbfc76bec756a9607788575c155483a41e
tyanocka <tyanocka@github.com>
2024-05-08 14:40:42 +0500

    fix: form component

--- resources/views/components/form.blade.php
+++ resources/views/components/form.blade.php
@@ -1,10 +1,10 @@
 <div class="mb-3">
     <label for="name" class="form-label">Имя</label>
-    <input type="text" class="form-control" id="name" name="name" value="{{ $phonebook ? $phonebook->name : '' }}">
+    <input type="text" class="form-control" id="name" name="name" value="{{ isset($phonebook) ? $phonebook->name : '' }}">
 </div>
 
 <div class="mb-3">
     <label for="phone" class="form-label">Номер телефона</label>
     <input type="text" class="form-control" id="phone" name="phone"
-        value="{{ $phonebook ? $phonebook->phone : '' }}">
+        value="{{ isset($phonebook) ? $phonebook->phone : '' }}">
 </div>
